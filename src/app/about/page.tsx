99d8d78f0b6d20f1455d8bf247aa3ac153c112a8
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-01-15 15:44:51 -0500

    Continue About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,37 +5,49 @@ import { variants } from '@/utils/animations';
 
 export default function About(): any {
   return (
-    <>
-      <motion.div
-        initial='hidden'
-        animate='visible'
-        transition={ { ease: 'easeInOut', duration: 0.8 } }
-        variants={ variants }
-        className={ `text-sky-100 text-9xl ${ cabinHeavy.className }` }
-      >
-        about.
-      </motion.div>
+    <div className={ 'flex flex-row' }>
+      <div className={'pr-8'}>
 
-      <motion.div
-        className={ `text-sky-200 text-3xl pb-12 -mt-2 ${ cabinLight.className }` }
-        initial='hidden'
-        animate='visible'
-        transition={ { ease: 'easeInOut', duration: 1 } }
-        variants={ variants }
-      >
-        This is who I am and where I&apos;m going.
-      </motion.div>
+        {/* TITLE */}
+        <motion.div
+          initial='hidden'
+          animate='visible'
+          transition={ { ease: 'easeInOut', duration: 0.6 } }
+          variants={ variants }
+          className={ `text-sky-100 text-9xl ${ cabinHeavy.className }` }
+        >
+          about
+        </motion.div>
 
-      <motion.div
-        className={ `text-sky-300 text-2xl` }
-        initial='hidden'
-        animate='visible'
-        transition={ { ease: 'easeInOut', duration: 1.2 } }
-        variants={ variants }
-      >
-        I&apos;ll talk about myself here, and about my previous career in education and whatnot. Then I&apos;ll talk about something else.
-      </motion.div>
-    </>
+        {/* SUBTITLE */}
+        <motion.div
+          className={ `text-sky-200 text-3xl pb-12 -mt-2 ${ cabinLight.className }` }
+          initial='hidden'
+          animate='visible'
+          transition={ { ease: 'easeInOut', duration: 0.8 } }
+          variants={ variants }
+        >
+          I&apos;m a full stack software engineer based in NYC
+        </motion.div>
+
+        {/* BODY TEXT */}
+        <motion.div
+          className={ `text-sky-300 text-2xl` }
+          initial='hidden'
+          animate='visible'
+          transition={ { ease: 'easeInOut', duration: 1 } }
+          variants={ variants }
+        >
+          <p>From the classrooms of New York City to the world of web development, I've spent nearly a decade shaping
+            minds as a public school teacher before transitioning to software development.</p>
+          <br />
+
+          <p>Now, as a dedicated full stack engineer with years of web development experience across various fields, I'm passionate about merging my educational insights with my technical skills to give back to the EdTech space.</p>
+        </motion.div>
+      </div>
+      {/*<img className={'mt-28 w-96 h-96'} src={'https://thispersondoesnotexist.com/'}/>*/}
+    </div>
+
   )
 }
 
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -4,36 +4,40 @@ import { cabinHeavy, cabinLight } from '@/utils/fonts';
 import { motion } from 'framer-motion';
 import { variants } from '@/utils/animations';
 
-export default function Portfolio(): ReactElement {return (
+export default function Portfolio(): ReactElement {
+  return (
     <>
+      { /* TITLE */ }
       <motion.div
         initial='hidden'
         animate='visible'
-        transition={ { ease: 'easeInOut', duration: 0.8 } }
+        transition={ { ease: 'easeInOut', duration: 0.6 } }
         variants={ variants }
         className={ `text-sky-100 text-9xl pb-4 ${ cabinHeavy.className }` }
       >
-        portfolio.
+        portfolio
       </motion.div>
 
+      { /* SUBTITLE */ }
       <motion.div
         className={ `text-sky-200 text-3xl pb-16 ${ cabinLight.className }` }
         initial='hidden'
         animate='visible'
-        transition={ { ease: 'easeInOut', duration: 1 } }
+        transition={ { ease: 'easeInOut', duration: 0.8 } }
         variants={ variants }
       >
-        Check out some of my latest work.
+        <p>Check out some of my latest work</p>
       </motion.div>
 
+      { /* BODY TEXT */ }
       <motion.div
         className={ `text-sky-300 text-2xl` }
         initial='hidden'
         animate='visible'
-        transition={ { ease: 'easeInOut', duration: 1.2 } }
-        variants={ variants }      >
-        This is where I&apos;ll display some of the projects that I&apos;m currently working on, along with some
-        screenshots.
+        transition={ { ease: 'easeInOut', duration: 1 } }
+        variants={ variants }>
+        <p>This is where I&apos;ll display some of the projects that I&apos;m currently working on, along with some
+          screenshots.</p>
       </motion.div>
     </>
   )
