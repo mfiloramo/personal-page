3b73c3ee9211ecede1fbb2fa1110e01ebb03ccb2
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-01-21 14:26:12 -0500

    Fix layout useclient bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-'use client';
 import React, { useEffect, useState } from 'react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
@@ -15,36 +14,27 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: {
   children: React.ReactNode
 }) {
-  const [ opacityClass, setOpacityClass ] = useState('opacity-0');
-
-  useEffect((): void => {
-    setOpacityClass('opacity-100 transition-opacity duration-1000 ease-in-out');
-  }, []);
-
   return (
     <html lang='en'>
     <body className={ `h-full ${ dm_sans.className }` }>
 
-    {/* APPLICATION TELEMETRY */}
+    {/* APPLICATION TELEMETRY */ }
     <SpeedInsights />
 
-    {/* FADE-IN WRAPPER */ }
-    <div className={ `${ opacityClass }` }>
-      {/* MAIN CONTAINER */ }
-      <div className='flex flex-col min-h-screen overflow-x-hidden'>
+    {/* MAIN CONTAINER */ }
+    <div className='flex flex-col min-h-screen overflow-x-hidden'>
 
-        {/* NAVBAR */ }
-        <Navbar />
+      {/* NAVBAR */ }
+      <Navbar />
 
-        {/* ROUTER */ }
-        <main
-          className='flex-grow overflow-y-auto shadow-[inset_0_6px_20px_rgba(0,0,0,0.6)] bg-blue-grid-background bg-cover bg-center'>
-          { children }
-        </main>
+      {/* ROUTER */ }
+      <main
+        className='flex-grow overflow-y-auto shadow-[inset_0_6px_20px_rgba(0,0,0,0.6)] bg-blue-grid-background bg-cover bg-center'>
+        { children }
+      </main>
 
-        {/* FOOTER */ }
-        <Footer />
-      </div>
+      {/* FOOTER */ }
+      <Footer />
     </div>
     </body>
     </html>
