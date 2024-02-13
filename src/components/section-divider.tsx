1ee128ed466a2746a9354e248e0d4cff8076612d
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-02-13 11:40:57 -0500

    Add additional tech stack icons

--- src/components/section-divider.tsx
+++ src/components/section-divider.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react';
 
 export default function SectionDivider(): ReactElement {
-  return <div className='border-t border-gray-400 mt-2 w-[90vw] max-w-[720px] mx-auto'></div>;
+  return <div className='border-t border-gray-400 mt-2 pb-4 w-[90vw] max-w-[720px] mx-auto'></div>;
 };
--- src/components/section.tsx
+++ src/components/section.tsx
@@ -8,10 +8,10 @@ export default function SectionComponent({ subtitle, paragraphs }: SectionCompon
     // SECTION CONTAINER
     <div className={ 'max-w-screen-md mx-auto px-8 first:pt-3' }>
       {/* SECTION SUBTITLE */ }
-      { subtitle && <h2 className={ 'text-3xl pb-3' }>{ subtitle }</h2> }
+      { subtitle && <h2 className={ 'text-3xl pb-1' }>{ subtitle }</h2> }
       {/* SECTION PARAGRAPHS */ }
       { paragraphs.map((paragraph: string, index: number) => (
-        <p className={ `${ index > 0 ? 'pt-6' : '' } last:pb-12 first:-mb-4 first:pt-8` } key={ index }>
+        <p className={ `${ index > 0 ? 'pt-6' : '' } last:pb-8 first:-mb-4 first:pt-8` } key={ index }>
           { paragraph }
         </p>
       )) }
