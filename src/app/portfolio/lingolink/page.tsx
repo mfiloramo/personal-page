76eb6382b0847dcf6580757d2168080b778cedb6
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-02-17 13:34:10 -0500

    Continue building lingolink hero-banner scaling

--- src/app/portfolio/lingolink/page.tsx
+++ src/app/portfolio/lingolink/page.tsx
@@ -11,7 +11,7 @@ import Image from 'next/image';
 export default function LingoLinkPage(): ReactElement {
   return (
     <div>
-      <div className='relative h-[50vh] flex justify-center items-center overflow-hidden section-shadow'>
+      <div className='relative h-[42vh] sm:h-[52vh] flex justify-center items-center overflow-hidden section-shadow'>
 
         {/* HERO BANNER IMAGE */ }
         <Image
@@ -21,106 +21,119 @@ export default function LingoLinkPage(): ReactElement {
           objectFit='cover'
         />
 
-        {/* ANIMATED LOGO GRAPHIC */ }
-        <motion.div
-          className='absolute mt-[4vh] inset-0 flex justify-center items-start'
-          initial={ { scale: 0 } }
-          animate={ { rotate: 360, scale: 1 } }
-          transition={ {
-            type: 'spring',
-            stiffness: 260,
-            damping: 20,
-          } }
-        >
-          <Image
-            className={ 'w-[15vw]' }
-            src='/images/lingolink-portfolio-page/lingolink-logo.png'
-            alt='LingoLink Logo'
-            width={ 240 }
-            height={ 240 }
-          />
-        </motion.div>
+        {/* ANIMATED LOGO CONTAINER */ }
+        <div className={ 'absolute mt-[4vh] inset-0 flex justify-center' }>
 
-        {/* ANIMATED LOGO TEXT */ }
-        <motion.div
-          className='absolute mt-64 inset-0 flex justify-center items-center'
-          initial={ { scale: 0 } }
-          animate={ { scale: 1 } }
-          transition={ {
-            delay: 0.2,
-            type: 'spring',
-            stiffness: 260,
-            damping: 20,
-          } }
-        >
-          <Image
-            src='/images/lingolink-portfolio-page/lingolink-text.png'
-            alt='LingoLink Logo Text'
-            width={ 375 }
-            height={ 375 }
-          />
-        </motion.div>
+          {/* ANIMATED LOGO GRAPHIC */ }
+          <motion.div
+            className='absolute inset-0 flex justify-center items-start'
+            initial={ { scale: 0 } }
+            animate={ { rotate: 360, scale: 1 } }
+            transition={ {
+              delay: 0.3,
+              type: 'spring',
+              stiffness: 260,
+              damping: 20,
+            } }
+          >
+            <Image
+              className={ 'w-[50vw] max-w-[240px]' }
+              src='/images/lingolink-portfolio-page/lingolink-logo.png'
+              alt='LingoLink Logo'
+              width={ 240 }
+              height={ 240 }
+            />
+          </motion.div>
+
+          {/* ANIMATED LOGO TEXT */ }
+          <div>
+            <motion.div
+              className='mt-64'
+              initial={ { scale: 0 } }
+              animate={ { scale: 1 } }
+              transition={ {
+                delay: 0.5,
+                type: 'spring',
+                stiffness: 260,
+                damping: 20,
+              } }
+            >
+              <Image
+                className={ 'max-w-[375px] -mt-5 w-[70vw]' }
+                src='/images/lingolink-portfolio-page/lingolink-text.png'
+                alt='LingoLink Logo Text'
+                width={ 375 }
+                height={ 375 }
+              />
+            </motion.div>
+          </div>
+        </div>
 
         {/* TEXT: MULTILINGUAL MESSAGING */ }
-        <motion.div
-          className='absolute bottom-0 right-0 flex justify-end items-end'
-          initial={ { x: '100vw', y: '100vh' } }
-          animate={ { x: 150, y: 150 } }
-          transition={ {
-            delay: 1,
-            type: 'spring',
-            stiffness: 200,
-            damping: 23,
-          } }
-        >
-          <Image
-            src='/images/lingolink-portfolio-page/text-multilingual-messaging.png'
-            alt='Description'
-            width={ 650 }
-            height={ 650 }
-          />
-        </motion.div>
+        <div className={ 'hidden xl:flex' }>
+          <motion.div
+            className='absolute bottom-0 right-0 justify-end items-end xl:flex'
+            initial={ { x: '100vw', y: '100vh' } }
+            animate={ { x: 150, y: 150 } }
+            transition={ {
+              delay: 1.2,
+              type: 'spring',
+              stiffness: 200,
+              damping: 23,
+            } }
+          >
+            <Image
+              src='/images/lingolink-portfolio-page/text-multilingual-messaging.png'
+              alt='Description'
+              width={ 650 }
+              height={ 650 }
+            />
+          </motion.div>
+        </div>
 
         {/* TEXT: MADE SIMPLE */ }
-        <motion.div
-          className='absolute bottom-0 right-0 flex justify-end items-end'
-          initial={ { x: '100vw', y: '100vh' } }
-          animate={ { x: 110, y: 170 } }
-          transition={ {
-            delay: 1.5,
-            type: 'spring',
-            stiffness: 200,
-            damping: 23,
-          } }
-        >
-          <Image
-            src='/images/lingolink-portfolio-page/text-made-simple.png'
-            alt='Description'
-            width={ 600 }
-            height={ 600 }
-          />
-        </motion.div>
+        <div className={ 'hidden xl:flex' }>
+          <motion.div
+            className='absolute bottom-0 right-0 justify-end items-end '
+            initial={ { x: '100vw', y: '100vh' } }
+            animate={ { x: 110, y: 170 } }
+            transition={ {
+              delay: 1.6,
+              type: 'spring',
+              stiffness: 200,
+              damping: 23,
+            } }
+          >
+            <Image
+              src='/images/lingolink-portfolio-page/text-made-simple.png'
+              alt='Description'
+              width={ 600 }
+              height={ 600 }
+            />
+          </motion.div>
+        </div>
 
         {/* ANIMATION: STATUS SIGN */ }
-        <motion.div
-          className='absolute left-52 flex justify-start items-end'
-          initial={ { y: '100vh' } }
-          animate={ { y: 60 } }
-          transition={ {
-            delay: 2,
-            type: 'spring',
-            stiffness: 260,
-            damping: 23,
-          } }
-        >
-          <Image
-            src='/images/lingolink-portfolio-page/text-status-sign.png'
-            alt='Description'
-            width={ 200 }
-            height={ 200 }
-          />
-        </motion.div>
-
+        <div className={ 'hidden xl:flex' }>
+          <motion.div
+            className='absolute bottom-7 left-52 justify-start items-startd'
+            initial={ { y: '100vh' } }
+            animate={ { y: 60 } }
+            transition={ {
+              delay: 2.1,
+              type: 'spring',
+              stiffness: 260,
+              damping: 23,
+            } }
+          >
+            <Image
+              src='/images/lingolink-portfolio-page/text-status-sign.png'
+              alt='Description'
+              width={ 200 }
+              height={ 200 }
+            />
+          </motion.div>
+        </div>
       </div>
 
 
@@ -139,6 +152,7 @@ export default function LingoLinkPage(): ReactElement {
           </div>
         )) }
       </div>
+
     </div>
   );
 }
