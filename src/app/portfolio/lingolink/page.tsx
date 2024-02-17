20adb8aa3c1c1e2d2a1bc236c5b213fd81e01865
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-02-17 08:20:13 -0500

    Add animated hero-banner to LingoLink portfolio page (need scaling/optim.)

--- src/app/portfolio/lingolink/page.tsx
+++ src/app/portfolio/lingolink/page.tsx
@@ -4,33 +4,132 @@ import pageContent from '../../../data/portfolio/pages/lingolink/lingolink-data.
 import stackContent from '../../../data/portfolio/pages/lingolink/lingolink-stack-data.json';
 import SectionDivider from '@/components/section-divider';
 import SectionComponent from '@/components/section';
-import ConstructionImage from '@/components/construction';
 import TechnologyStackComponent from '@/components/technology-stack';
 import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+import Image from 'next/image';
 
 export default function LingoLinkPage(): ReactElement {
-  // Hook to detect when the div is in view
-  const { ref, inView } = useInView({
-    triggerOnce: false, // Trigger animation only once
-    threshold: 0.5, // Trigger when 50% of the div is in view
-  });
-
   return (
     <div>
-      <ConstructionImage />
+      <div className='relative h-[50vh] flex justify-center items-center overflow-hidden section-shadow'>
 
-      {/* PAGE SECTIONS */ }
-      <div className={ `mx-auto text-xl` }>
+        {/* HERO BANNER IMAGE */ }
+        <Image
+          src={ `/images/hero-banners/hero-banner-lingolink.png` }
+          alt={ 'LingoLink Banner' }
+          layout='fill'
+          objectFit='cover'
+        />
+
+        {/* ANIMATED LOGO GRAPHIC */ }
+        <motion.div
+          className='absolute mt-[4vh] inset-0 flex justify-center items-start'
+          initial={ { scale: 0 } }
+          animate={ { rotate: 360, scale: 1 } }
+          transition={ {
+            type: 'spring',
+            stiffness: 260,
+            damping: 20,
+          } }
+        >
+          <Image
+            className={ 'w-[15vw]' }
+            src='/images/lingolink-portfolio-page/lingolink-logo.png'
+            alt='LingoLink Logo'
+            width={ 240 }
+            height={ 240 }
+          />
+        </motion.div>
+
+        {/* ANIMATED LOGO TEXT */ }
+        <motion.div
+          className='absolute mt-64 inset-0 flex justify-center items-center'
+          initial={ { scale: 0 } }
+          animate={ { scale: 1 } }
+          transition={ {
+            delay: 0.2,
+            type: 'spring',
+            stiffness: 260,
+            damping: 20,
+          } }
+        >
+          <Image
+            src='/images/lingolink-portfolio-page/lingolink-text.png'
+            alt='LingoLink Logo Text'
+            width={ 375 }
+            height={ 375 }
+          />
+        </motion.div>
+
+        {/* TEXT: MULTILINGUAL MESSAGING */ }
+        <motion.div
+          className='absolute bottom-0 right-0 flex justify-end items-end'
+          initial={ { x: '100vw', y: '100vh' } }
+          animate={ { x: 150, y: 150 } }
+          transition={ {
+            delay: 1,
+            type: 'spring',
+            stiffness: 200,
+            damping: 23,
+          } }
+        >
+          <Image
+            src='/images/lingolink-portfolio-page/text-multilingual-messaging.png'
+            alt='Description'
+            width={ 650 }
+            height={ 650 }
+          />
+        </motion.div>
+
+        {/* TEXT: MADE SIMPLE */ }
+        <motion.div
+          className='absolute bottom-0 right-0 flex justify-end items-end'
+          initial={ { x: '100vw', y: '100vh' } }
+          animate={ { x: 110, y: 170 } }
+          transition={ {
+            delay: 1.5,
+            type: 'spring',
+            stiffness: 200,
+            damping: 23,
+          } }
+        >
+          <Image
+            src='/images/lingolink-portfolio-page/text-made-simple.png'
+            alt='Description'
+            width={ 600 }
+            height={ 600 }
+          />
+        </motion.div>
+
+        {/* ANIMATION: STATUS SIGN */ }
+        <motion.div
+          className='absolute left-52 flex justify-start items-end'
+          initial={ { y: '100vh' } }
+          animate={ { y: 60 } }
+          transition={ {
+            delay: 2,
+            type: 'spring',
+            stiffness: 260,
+            damping: 23,
+          } }
+        >
+          <Image
+            src='/images/lingolink-portfolio-page/text-status-sign.png'
+            alt='Description'
+            width={ 200 }
+            height={ 200 }
+          />
+        </motion.div>
+
+      </div>
+
+
+      {/* PAGE CONTENT SECTIONS */ }
+      <div className='mx-auto text-xl'>
         { pageContent.sections.map((section: any, index: number) => (
           <div key={ index }>
-            {/* SECTION COMPONENT */ }
             <SectionComponent subtitle={ section.subtitle } paragraphs={ section.paragraphs } />
-
-            {/* PAGE DIVIDER */ }
             { index !== pageContent.sections.length - 1 && index !== 3 && <SectionDivider /> }
-
-            {/* TECHNOLOGY STACK SECTION */ }
             { index === 3 && (
               <TechnologyStackComponent
                 introduction={ pageContent.text.technologyStackIntro }
@@ -39,39 +138,6 @@ export default function LingoLinkPage(): ReactElement {
             ) }
           </div>
         )) }
-
-        {/* ANIMATED BLOCK CONTAINER */ }
-        <div className="flex justify-center items-center space-x-4">
-
-          {/* ANIMATED BLOCK */ }
-          <motion.div
-            ref={ ref }
-            animate={ { x: inView ? '0vw' : '-50vw' } }
-            initial={ { x: '-100vw' } }
-            transition={ {
-              type: 'spring',
-              stiffness: 260,
-              damping: 20,
-            } }
-          >
-            <img src={'/images/technology-icons/tech-sequelize.png'} alt={'p'} width={'100px'} />
-          </motion.div>
-
-          {/* ANIMATED BLOCK */ }
-          <motion.div
-            ref={ ref }
-            animate={ { x: inView ? '0vw' : '42vw' } }
-            initial={ { x: '100vw' } }
-            transition={ {
-              type: 'spring',
-              stiffness: 260,
-              damping: 20,
-            } }
-            className="bg-white p-12 mb-10 w-3"
-          >
-          </motion.div>
-        </div>
-
       </div>
     </div>
   );
--- src/components/section-divider.tsx
+++ src/components/section-divider.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react';
 
 export default function SectionDivider(): ReactElement {
-  return <div className='border-t border-gray-400 mt-2 pb-4 mx-auto'></div>;
+  return <div className='border-t border-gray-400 mt-2 pb-4 mx-auto w-[90vw] max-w-screen-md'></div>;
 };
