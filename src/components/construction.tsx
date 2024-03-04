786375b003b92d21ca5709db4340bb743bd6d178
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-03-03 20:26:26 -0500

    Fix spacing

--- src/components/construction.tsx
+++ src/components/construction.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Construction(): ReactElement {
   return (
     <Image
-      className={ 'shadow-2xl mt-4 max-w-screen-sm mx-auto  w-[70vw] sm:w-[50vw]' }
+      className={ 'shadow-2xl mt-4 max-w-screen-sm mx-auto w-[70vw] sm:w-[50vw]' }
       src='/images/construction.png'
       alt='Construction Image'
       width={ 640 }
