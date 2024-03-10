907f2d5446b9ad8f754e411ac9dc1e70c780de63
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-03-10 17:51:29 -0400

    Add note to modularize

--- src/app/portfolio/lingolink/page.tsx
+++ src/app/portfolio/lingolink/page.tsx
@@ -16,6 +16,7 @@ export default function LingoLinkPage(): ReactElement {
            style={ { backgroundColor: '#000' } }>
         {/* TODO: FIX LINGOLINK LOGO SIZING (CLIPS ON SMALLER-Y VIEWPORTS) */ }
 
+        {/* TODO: MODULARIZE HERO BANNER TO COMPONENT */}
         {/* HERO BANNER IMAGE */ }
         <motion.div
           initial={ { opacity: 0 } }
@@ -107,7 +108,7 @@ export default function LingoLinkPage(): ReactElement {
         </div>
 
         {/* TEXT: MADE SIMPLE */ }
-        <div className={ 'hidden xl:flex' }>
+        <div className={ 'hidden  xl:flex' }>
           <motion.div
             className='absolute bottom-0 right-0 justify-end items-end '
             initial={ { x: '100vw', y: '100vh' } }
