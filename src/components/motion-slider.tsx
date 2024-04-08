b430061f2d6499b3488f0ee376ec54316877aeb7
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-04-08 03:56:03 -0400

    Increase blurring in MotionSlider component

--- src/components/motion-slider.tsx
+++ src/components/motion-slider.tsx
@@ -6,7 +6,7 @@ import { TextProps } from '@/interfaces/TextProps.interface';
 export const MotionSlider = ({ children, className, delay }: TextProps) => {
   return (
     <motion.div
-      initial={{ x: -130, opacity: 0, filter: 'blur(4px)' }}
+      initial={{ x: -130, opacity: 0, filter: 'blur(12px)' }}
       animate={{ x: 0, opacity: 1, filter: 'blur(0px)' }}
       transition={ { ease: 'easeInOut', duration: 0.6 + delay } }
       variants={ variants }
