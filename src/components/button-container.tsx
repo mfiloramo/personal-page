08dc0241fd705290b8b1e416910088679d9130bc
Michael Filoramo <mlfiloramo@gmail.com>
2024-12-04 12:13:52 -0500

    Modernize title buttons

--- src/components/button-container.tsx
+++ src/components/button-container.tsx
@@ -22,7 +22,7 @@ export default function ButtonContainer({ buttonOneText, buttonTwoText, buttonOn
           target={ buttonOneText === 'Portfolio' ? '' : '_blank' }
         >
           <motion.div
-            className={ `bg-gradient-to-b from-slate-100 to-slate-400 rounded-xl text-slate-950 py-3 px-7 min-w-[230px] text-3xl cursor-pointer border-2 border-black shadow-xl hover:shadow-2xl w-full sm:w-auto my-2 text-center ${ inter_tightHeavy.className }` }
+            className={ `bg-gradient-to-b bg-slate-300 rounded-lg text-slate-950 py-3 px-7 min-w-[230px] text-3xl cursor-pointer border-2 border-black shadow-xl hover:shadow-2xl w-full sm:w-auto my-2 text-center ${ inter_tightHeavy.className }` }
             whileHover={ { scale: 1.05 } }
             whileTap={ { scale: 0.95 } }
             transition={ { duration: 0.1 } }
@@ -36,7 +36,7 @@ export default function ButtonContainer({ buttonOneText, buttonTwoText, buttonOn
       <MotionSlider className={ '' } delay={ 1.4 }>
         <Link href={ buttonTwoLink! ? buttonTwoLink!.toLowerCase() : buttonTwoText!.toLowerCase() } rel={ buttonTwoText === 'Services' ? '' : 'noopener noreferrer' } target={ buttonTwoText === 'Services' ? '' : '_blank' }>
           <motion.div
-            className={ `bg-gradient-to-b from-slate-100 to-slate-400 rounded-xl text-slate-950 py-3 px-7 min-w-[230px] text-3xl cursor-pointer border-2 border-black shadow-xl hover:shadow-2xl w-full sm:w-auto my-2 text-center ${ inter_tightHeavy.className }` }
+            className={ `bg-gradient-to-b bg-slate-300 rounded-lg text-slate-950 py-3 px-7 min-w-[230px] text-3xl cursor-pointer border-2 border-black shadow-xl hover:shadow-2xl w-full sm:w-auto my-2 text-center ${ inter_tightHeavy.className }` }
             whileHover={ { scale: 1.05 } }
             whileTap={ { scale: 0.95 } }
             transition={ { duration: 0.1 } }
